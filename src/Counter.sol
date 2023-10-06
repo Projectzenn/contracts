83d7fce87d165f40a03706a3b8172d2a39197cd8
koenschuite <tse.lao@yahoo.com>
2023-10-06 18:55:22 +0200

    starting

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+contract CompanyStructure {
     uint256 public number;
+    
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
 }
